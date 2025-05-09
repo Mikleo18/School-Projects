367146af849eec7a4cc509c06f5ae752efd5b79d
Mikleo <seyupaltas02@gmail.com>
2025-05-09 18:14:26 +0300

    MemberRegistration

---
+++ CSharp/MemberRegistration/MemberRegistration.Business/Concrete/MemberManager.cs
@@ -0,0 +1,50 @@
+﻿using DevFramework.Core.Aspects.Postsharp.ValidationAspects;
+using MemberRegistration.Business.Abstract;
+using MemberRegistration.Business.ServiceAdapters;
+using MemberRegistration.Business.ValidationRules.FluentValidation;
+using MemberRegistration.DataAccess.Abstract;
+using MemberRegistration.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegistration.Business.Concrete
+{
+    public class MemberManager : IMemberService
+    {
+        private IMemberDal _memberDal;
+        private IKpsService _kpsService;
+
+        public MemberManager(IMemberDal memberDal, IKpsService kpsService)
+        {
+            _memberDal = memberDal;
+            _kpsService = kpsService;
+        }
+
+        [FluentValidationAspect(typeof(MemberValidator))]
+        public void Add(Member member)
+        {
+            CheckIfMemberExists(member);
+            CheckIfUserValidFromKps(member);
+            _memberDal.Add(member);
+        }
+
+        private void CheckIfUserValidFromKps(Member member)
+        {
+            if (_kpsService.ValidateUser(member) == false)
+            {
+                throw new Exception("Kullanıcı Doğrulaması Geçerli Değil");
+            }
+        }
+
+        private void CheckIfMemberExists(Member member)
+        {
+            if (_memberDal.Get(m => m.TcNo == member.TcNo) != null)
+            {
+                throw new Exception("Bu Kullanıcı Daha Önce Kayıt Olmuştur");
+            }
+        }
+    }
+}
---
+++ CSharp/MemberRegistration/MemberRegistration.ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using MemberRegistration.Business.Abstract;
+using MemberRegistration.Business.DependecyResolvers.Ninject;
+using MemberRegistration.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegistration.ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var memberService = InstanceFactory.GetInstance<IMemberService>();
+            memberService.Add(new Member { Email="seyupaltas02@gmail.com",FirstName="Selahattin",LastName="Altaş",DateOfBirth=new DateTime(2002,11,26),TcNo="57958290342"});
+            Console.WriteLine("Eklendi");
+            Console.ReadLine();
+        }
+    }
+}
