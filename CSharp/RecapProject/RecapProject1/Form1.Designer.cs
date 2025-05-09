945413193cd25b19c9b402d29d27dd4d7bc86fca
Mikleo <seyupaltas02@gmail.com>
2025-05-09 18:21:08 +0300

    Recap

---
+++ CSharp/RecapProject/RecapProject1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace RecapProject1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxCategory = new System.Windows.Forms.GroupBox();
+            this.lblKategori = new System.Windows.Forms.Label();
+            this.cbxCategory = new System.Windows.Forms.ComboBox();
+            this.gbxSearch = new System.Windows.Forms.GroupBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.tbxSearch = new System.Windows.Forms.TextBox();
+            this.dgwProduct = new System.Windows.Forms.DataGridView();
+            this.gbxCategory.SuspendLayout();
+            this.gbxSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxCategory
+            // 
+            this.gbxCategory.Controls.Add(this.lblKategori);
+            this.gbxCategory.Controls.Add(this.cbxCategory);
+            this.gbxCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gbxCategory.Location = new System.Drawing.Point(12, 12);
+            this.gbxCategory.Name = "gbxCategory";
+            this.gbxCategory.Size = new System.Drawing.Size(1088, 100);
+            this.gbxCategory.TabIndex = 0;
+            this.gbxCategory.TabStop = false;
+            this.gbxCategory.Text = "Kategoriye Göre Listele";
+            // 
+            // lblKategori
+            // 
+            this.lblKategori.AutoSize = true;
+            this.lblKategori.Location = new System.Drawing.Point(43, 45);
+            this.lblKategori.Name = "lblKategori";
+            this.lblKategori.Size = new System.Drawing.Size(65, 17);
+            this.lblKategori.TabIndex = 1;
+            this.lblKategori.Text = "Kategori:";
+            // 
+            // cbxCategory
+            // 
+            this.cbxCategory.FormattingEnabled = true;
+            this.cbxCategory.Location = new System.Drawing.Point(119, 42);
+            this.cbxCategory.Name = "cbxCategory";
+            this.cbxCategory.Size = new System.Drawing.Size(442, 24);
+            this.cbxCategory.TabIndex = 0;
+            this.cbxCategory.SelectedIndexChanged += new System.EventHandler(this.cbxCategory_SelectedIndexChanged);
+            // 
+            // gbxSearch
+            // 
+            this.gbxSearch.Controls.Add(this.lblSearch);
+            this.gbxSearch.Controls.Add(this.tbxSearch);
+            this.gbxSearch.Location = new System.Drawing.Point(12, 118);
+            this.gbxSearch.Name = "gbxSearch";
+            this.gbxSearch.Size = new System.Drawing.Size(1088, 100);
+            this.gbxSearch.TabIndex = 1;
+            this.gbxSearch.TabStop = false;
+            this.gbxSearch.Text = "Ürün İsmine Göre Listele";
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(46, 43);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(35, 16);
+            this.lblSearch.TabIndex = 1;
+            this.lblSearch.Text = "Ürün";
+            // 
+            // tbxSearch
+            // 
+            this.tbxSearch.Location = new System.Drawing.Point(119, 38);
+            this.tbxSearch.Name = "tbxSearch";
+            this.tbxSearch.Size = new System.Drawing.Size(442, 22);
+            this.tbxSearch.TabIndex = 0;
+            this.tbxSearch.TextChanged += new System.EventHandler(this.tbxSearch_TextChanged);
+            // 
+            // dgwProduct
+            // 
+            this.dgwProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwProduct.Location = new System.Drawing.Point(12, 240);
+            this.dgwProduct.Name = "dgwProduct";
+            this.dgwProduct.RowHeadersWidth = 51;
+            this.dgwProduct.RowTemplate.Height = 24;
+            this.dgwProduct.Size = new System.Drawing.Size(1088, 365);
+            this.dgwProduct.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 658);
+            this.Controls.Add(this.dgwProduct);
+            this.Controls.Add(this.gbxSearch);
+            this.Controls.Add(this.gbxCategory);
+            this.Name = "Form1";
+            this.Text = "Ürünler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxCategory.ResumeLayout(false);
+            this.gbxCategory.PerformLayout();
+            this.gbxSearch.ResumeLayout(false);
+            this.gbxSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxCategory;
+        private System.Windows.Forms.Label lblKategori;
+        private System.Windows.Forms.ComboBox cbxCategory;
+        private System.Windows.Forms.GroupBox gbxSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox tbxSearch;
+        private System.Windows.Forms.DataGridView dgwProduct;
+    }
+}
+
---
+++ CSharp/RecapProject/RecapProject1/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using RecapProject1.Entities;
+
+namespace RecapProject1
+{
+    public class NorthwindContext:DbContext
+    {
+        public DbSet<Product> products {  get; set; }
+
+        public DbSet<Category> categories { get; set; }
+
+    }
+}
